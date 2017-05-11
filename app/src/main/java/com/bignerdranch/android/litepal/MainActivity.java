dbb61507d055d6d54ef6a65d9334c2499ad6fce3
wanglianghai <1540446221@qq.com>
2017-05-11 09:53:41 +0800

    update

--- app/src/main/java/com/bignerdranch/android/litepal/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/litepal/MainActivity.java
@@ -3,23 +3,40 @@ package com.bignerdranch.android.litepal;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.LitePal;
 import org.litepal.crud.DataSupport;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private Button mButtonCreate;
     private Button mButtonInsert;
     private Button mButtonUpdate;
     private Button mButtonConditionUpdate;
     private Button mButtonDelete;
+    private Button mButtonQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonQuery = (Button) findViewById(R.id.query_book);
+        mButtonQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Category> categories = DataSupport.where("mCategoryCode = ?", "123").order("mId").find(Category.class);
+                for (Category c:
+                     categories) {
+                    Log.i(TAG, "onClick: " + c.getCategoryName() + " " + c.getId() + " " + c.getCategoryCode());
+                }
+            }
+        });
 
         mButtonDelete = (Button) findViewById(R.id.delete_book);
         mButtonDelete.setOnClickListener(new View.OnClickListener() {
@@ -66,3 +83,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
